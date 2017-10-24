cfaeffa6c6c5dc3f884bf01d3dc57040898413fc
b27676110 <105021036@live.asia.edu.tw>
2017-10-24 12:57:10 +0800

    20171024_105021036

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -9,6 +9,11 @@ public class LoginFrame extends JFrame {
     private JButton jbn2 = new JButton("Go");
     private JLabel jbl1 = new JLabel("ID");
     private JLabel jbl2 = new JLabel("Password");
+    private JTextField jfID = new JTextField();
+    private JPasswordField jfPW = new JPasswordField();
+    private int screenW = Toolkit.getDefaultToolkit().getScreenSize().width;
+    private int screenH = Toolkit.getDefaultToolkit().getScreenSize().height;
+    private int frmW=300,frmH=150;
 
 
     public LoginFrame(){
@@ -17,13 +22,28 @@ public class LoginFrame extends JFrame {
     private void Init() {
         cp = this.getContentPane();
         cp.setLayout(new GridLayout(3,2,1,1));
+        this.setBounds((screenW-frmW)/2,(screenH-frmH)/2,frmW,frmH);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         cp.add(jbl1);
-        cp.add();
+        cp.add(jfID);
         cp.add(jbl2);
-        cp.add();
+        cp.add(jfPW);
         cp.add(jbn1);
         cp.add(jbn2);
+
+        jbn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        jbn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    if(jfID.getText().equals("h304")&&
+                            (new String(jfPW.getPassword())).equals("23323"))
+            }
+        });
 
 
     }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         LoginFrame login = new LoginFrame();
+        login.setVisible(true);
 
 
     }
